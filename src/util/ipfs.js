1d2ca97eed1cd0b9988a3aa54aca82464a5081ad
KH Kwan <ecorpnu@gmail.com>
2022-12-30 00:11:43 +1100

    Update ipfs.js
    As of Aug 2022, infura deprecated their free gateway and requires authentication plus a credit card. At the same time 'ipfs-api' is also deprecated and now is ipfs-http-client. However, you need to download version 33.0.1. You can see some of the pain points at https://community.infura.io/t/ipfs-authentication-error-403-forbidden/5672/83

--- src/util/ipfs.js
+++ src/util/ipfs.js
@@ -1,6 +1,20 @@
 //using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
-const IPFS = require('ipfs-api');
-const ipfs = new IPFS({host: 'ipfs.infura.io', port: 5001, protocol: 'https'});
+const ipfsClient = require('ipfs-http-client');
+//use version npm i ipfs-http-client@33.0.1
+//need to open an IPFS project with infura.
+const projectId ='some stuff';
+const projectSecret='some stuff';
+const auth = 'Basic ' + Buffer.from(projectId + ':' +projectSecret).toString('base64');
+
+const ipfs = new ipfsClient({  
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    apiPath: '/api/v0',
+    headers: {
+      authorization: auth
+    }
+  })
 
 //run with local daemon
 // const ipfsApi = require('ipfs-api');
